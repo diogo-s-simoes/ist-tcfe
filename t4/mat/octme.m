72a04d47c1928f5d12c46e5e3e36d34597e58d0d
Diogo <diogo.s.simoes@tecnico.ulisboa.pt>
2021-05-20 18:43:13 +0100

    me

---
+++ t4/mat/octme.m
@@ -0,0 +1,22 @@
+Vc = 12.0
+f=1e3
+Vs = 10e-3
+Rs = 100
+Ci = 1e-3
+Rb1 = 80e3
+Rb2 =20e3
+Rc = 1e3
+Re =100
+Ce = 1e-3
+
+%Q1
+VBEON=0.7
+VT=25e-3
+BFN=178.7
+VAFN=69.7
+
+%Q2
+
+Rd = 100
+Co = 1e-6
+Rl = 8
