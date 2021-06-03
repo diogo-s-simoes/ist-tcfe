baada155b354bb830f1f83506ce303df794dc754
Diogo <diogo.s.simoes@tecnico.ulisboa.pt>
2021-06-03 18:42:00 +0100

    bliiklio

--- t5/mat/oct.m
+++ t5/mat/oct.m
@@ -11,20 +11,37 @@ wl=1/(R1*C1)
 wh=1/(R2*C2)
 wo=sqrt(wh*wl)
 
+# 2nd part
 
 w=wo
-Zi=75
-Zo=2*10^6
+Zo=75
+Zi=2e6
 Ao=10
-
 Vi=1
 
+f=logspace(1,8,70)
+res = zeros(1,length(f))
+
+for i=1:length(f)
+w=2*pi*f(i)
 A = [C1*w*i+1/R1+1/Zi,0,0,-1/Zi,0;
-Ao,1,0,-Ao,0;
+-Ao,1,0,Ao,0;
 0,-1/Zo,1/R2+1/Zo+1/R3,-1/R3,-1/R2;
 -1/Zi,0,-1/R3,1/R3+1/R4+1/Zi,0;
 0,0,-1/R2,0,1/R2+C2*w*i]
     
-B=[Vi*C1*w*i,0,0,0,0]
+B=[Vi*C1*w*i;0;0;0;0]
 
-V=A/B
+V=A\B
+
+
+res(i)=V(5)
+
+endfor
+
+semilogx(f, 20*log(abs(res)), "color", 'b')
+title("Gain (dB)")
+xlabel("f(Hz)")
+ylabel("V(dB)")
+
+print -color -depsc t5dB.eps
